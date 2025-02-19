7caa305a2c8091cb149dcff58ca7520e21817be5
PhelpsKP <150376728+PhelpsKP@users.noreply.github.com>
2025-02-18 23:55:02 -0500

    Absolute import for the logo

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,11 +1,12 @@
 import React from 'react';
+import logo from '../images/logo.png';
 
 function Header() {
   return (
     <header>
       <h1>
-        <a className="logo" href="index.html">
-          <img alt="Home" src="/images/logo.png" />
+        <a className="logo" href="https://www.jungle-hub.com">
+          <img alt="Home" src={logo} />
         </a>
       </h1>
     </header>
